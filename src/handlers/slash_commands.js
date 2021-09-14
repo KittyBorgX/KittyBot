794f4b777491217c497264eb7cbb9053035f7afc
Krishna Ramasimha <krishnaramasimha12@gmail.com>
2021-09-14 19:28:01 +0530

    Add files

--- src/handlers/slash_commands.js
+++ src/handlers/slash_commands.js
@@ -8,12 +8,12 @@ const { readdirSync } = require("fs");
 
 module.exports = async (client) => {
   const array = [];
-  readdirSync("./application_commands/").forEach((dir) => {
-    const commands = readdirSync(`./application_commands/${dir}/`).filter(
-      (files) => files.endsWith(".js")
+  readdirSync("./slash_commands/").forEach((dir) => {
+    const commands = readdirSync(`./slash_commands/${dir}/`).filter((files) =>
+      files.endsWith(".js")
     );
     for (const file of commands) {
-      const command = require(`../application_commands/${dir}/${file}`);
+      const command = require(`../slash_commands/${dir}/${file}`);
       if (!command.name) throw new Error("Please provide a slash command name");
       if (!command.description)
         throw new Error("Please provide a slash command description");
